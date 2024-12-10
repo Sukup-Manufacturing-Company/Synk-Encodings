078c95dce2b36fcd4fbbd3aa28c64fa6f9fba31f
Nathanial Boehlje <nboehlje@sukup.com>
2024-12-10 13:13:02 -0600

    Support for decoding/parsing string (#2)

--- src/Synk.Encodings.Crockford/CrockfordUuid.cs
+++ src/Synk.Encodings.Crockford/CrockfordUuid.cs
@@ -1,65 +1,57 @@
 ﻿using System.Text;
-using Synk.Encodings.Crockford.Helpers; 
 
 namespace Synk.Encodings.Crockford;
 public readonly struct CrockfordUuid
 {
-    private readonly byte[] CrockfordChars = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"u8.ToArray(); 
-    private readonly string _str; 
-    // TODO: use a fixed size char array instead of a string
-    private readonly byte[] _encodedBytes = new byte[26]; 
-    public readonly int Length => _str.Length; 
+    private static readonly char[] CrockfordChars = [.. "0123456789ABCDEFGHJKMNPQRSTVWXYZ"]; 
+    private readonly char[] _encodedChars = new char[26];
+    public readonly int Length => _encodedChars.Length; 
     public static readonly CrockfordUuid Empty; 
     private CrockfordUuid(byte[] bytes)    
     {
-        _str = EncodeSegmented(input: bytes, separater: '-', segmentLength: 4); 
+        EncodeCharacters(bytes, ref _encodedChars); 
     }
     public override readonly string ToString()
     {
-        return _str; 
+        return GetFormattedString(_encodedChars, separater: '-', segmentLength: 4); 
     }
-    public readonly string ToString(char separater, int segmentLength)
+    public string ToString(char separater, int segmentLength)
     {
-       _str.ToCharArray(); 
+        return GetFormattedString(_encodedChars, separater, segmentLength); 
     }
     public static CrockfordUuid FromGuid(Guid guid) => new(guid.ToByteArray()); 
-    private ReadOnlySpan<char> Encode(byte[] input)
+    private static void EncodeCharacters(byte[] source, ref char[] dest)
     {
-        ArgumentNullException.ThrowIfNull(input); 
+        ArgumentNullException.ThrowIfNull(source); 
 
-        StringBuilder sb = new();
-        for (int offset = 0; offset < input.Length;)
+        // write the contents of the input array into the 
+        // array of Crockford's Base32 encoded characters
+        WriteEncodedCharacters(source, dest); 
+
+        // remove any leading zeros 
+        int leadingZeroCount = 0; 
+        foreach (var num in dest)
         {
-            byte a, b, c, d, e, f, g, h;
-            int numCharsToOutput = GetNextGroup(input, ref offset, out a, out b, out c, out d, out e, out f, out g, out h);
-            _encodedBytes[0] = CrockfordChars[a]; 
-            sb.AppendWhen(numCharsToOutput >= 1, CrockfordChars[a]); 
-            sb.AppendWhen(numCharsToOutput >= 2, CrockfordChars[b]);
-            sb.AppendWhen(numCharsToOutput >= 3, CrockfordChars[c]);
-            sb.AppendWhen(numCharsToOutput >= 4, CrockfordChars[d]);
-            sb.AppendWhen(numCharsToOutput >= 5, CrockfordChars[e]); 
-            sb.AppendWhen(numCharsToOutput >= 6, CrockfordChars[f]);
-            sb.AppendWhen(numCharsToOutput >= 7, CrockfordChars[g]);  
-            sb.AppendWhen(numCharsToOutput >= 8, CrockfordChars[h]);
-        }
+            if (num is not '0')
+            {
+                break; 
+            }
 
-        return sb.ToString().TrimStart('0'); 
+            leadingZeroCount++; 
+        }
+        dest = dest.AsSpan()[leadingZeroCount..].ToArray(); 
     }
     // Create formatted string -> FXAG-K3WP-QRV4-H2TV-DPTB-SDJJ-40
-    private static string EncodeSegmented(byte[] input, char separater, int segmentLength)
+    private static string GetFormattedString(ReadOnlySpan<char> input, char separater, int segmentLength)
     {
-        ArgumentNullException.ThrowIfNull(input); 
-
-        ReadOnlySpan<char> encoded = Encode(input); 
         StringBuilder sb = new(); 
-
-        for (int i = 0; i < encoded.Length;)
+        for (int i = 0; i < input.Length;)
         {
-            int sliceLength = i + segmentLength > encoded.Length ? encoded.Length - i : segmentLength; 
-            sb.Append(encoded.Slice(i, sliceLength)); 
+            int sliceLength = i + segmentLength > input.Length ? input.Length - i : segmentLength; 
+            sb.Append(input.Slice(i, sliceLength)); 
             i += sliceLength; 
 
-            if (i < encoded.Length)
+            if (i < input.Length)
             {
                 sb.Append(separater); 
             }
@@ -85,7 +77,7 @@ public readonly struct CrockfordUuid
         int outputIndex = 0;
         while (outputIndex < output.Length)
         {
-            int byteIndex = CrockfordChars.IndexOf(input[inputIndex]);
+            int byteIndex = Array.IndexOf(CrockfordChars, input[inputIndex]);
             if (byteIndex < 0)
             {
                 throw new FormatException();
@@ -112,33 +104,48 @@ public readonly struct CrockfordUuid
 
         return new CrockfordUuid(output); 
     }
-    private static int GetNextGroup(byte[] input, ref int offset, out byte a, out byte b, out byte c, out byte d, out byte e, out byte f, out byte g, out byte h)
+    private static int WriteEncodedCharacters(byte[] source, char[] dest)
     {
-        uint b1, b2, b3, b4, b5;
-        int retVal = (input.Length - offset) switch 
+        int destIndex = 0; 
+        for (int srcIndex = 0; srcIndex < source.Length;)
         {
-            1 => 2, 
-            2 => 4, 
-            3 => 5,
-            4 => 7,
-            _ => 8 
-        }; 
+            int bytesToWrite = (source.Length - srcIndex) switch 
+            {
+                1 => 2, 
+                2 => 4, 
+                3 => 5,
+                4 => 7,
+                _ => 8 
+            }; 
 
-        b1 = (offset < input.Length) ? input[offset++] : 0U;
-        b2 = (offset < input.Length) ? input[offset++] : 0U;
-        b3 = (offset < input.Length) ? input[offset++] : 0U;
-        b4 = (offset < input.Length) ? input[offset++] : 0U;
-        b5 = (offset < input.Length) ? input[offset++] : 0U;
+            uint b1, b2, b3, b4, b5;
+            b1 = (srcIndex < source.Length) ? source[srcIndex++] : 0U;
+            b2 = (srcIndex < source.Length) ? source[srcIndex++] : 0U;
+            b3 = (srcIndex < source.Length) ? source[srcIndex++] : 0U;
+            b4 = (srcIndex < source.Length) ? source[srcIndex++] : 0U;
+            b5 = (srcIndex < source.Length) ? source[srcIndex++] : 0U;
 
-        a = (byte)(b1 >> 3);
-        b = (byte)(((b1 & 0x07) << 2) | (b2 >> 6));
-        c = (byte)((b2 >> 1) & 0x1f);
-        d = (byte)(((b2 & 0x01) << 4) | (b3 >> 4));
-        e = (byte)(((b3 & 0x0f) << 1) | (b4 >> 7));
-        f = (byte)((b4 >> 2) & 0x1f);
-        g = (byte)(((b4 & 0x3) << 3) | (b5 >> 5));
-        h = (byte)(b5 & 0x1f);
+            byte a, b, c, d, e, f, g, h; 
+            a = (byte)(b1 >> 3); 
+            b = (byte)(((b1 & 0x07) << 2) | (b2 >> 6)); 
+            c = (byte)((b2 >> 1) & 0x1f); 
+            d = (byte)(((b2 & 0x01) << 4) | (b3 >> 4)); 
+            e = (byte)(((b3 & 0x0f) << 1) | (b4 >> 7)); 
+            f = (byte)((b4 >> 2) & 0x1f);
+            g = (byte)(((b4 & 0x3) << 3) | (b5 >> 5));
+            h = (byte)(b5 & 0x1f);
 
-        return retVal;
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[a]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[b]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[c]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[d]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[e]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[f]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[g]; 
+            if (bytesToWrite-- is > 0) dest[destIndex++] = CrockfordChars[h]; 
+        }
+
+        int bytesWritten = destIndex + 1; 
+        return bytesWritten;
     }
 }
--- tests/Synk.Encodings.Tests/CrockfordUuidTests.cs
+++ tests/Synk.Encodings.Tests/CrockfordUuidTests.cs
@@ -7,7 +7,12 @@ public class CrockfordUuidTests
     [Fact]
     public void Parse_Correctly_Parses_The_Input_String()
     {
-        var uuid = CrockfordUuid.Parse("M7KA-0TM3-2ACM-X6NX-BRCY-712R-FM"); 
-        Console.WriteLine(uuid.ToString()); 
+        string expected = "M7KA-0TM3-2ACM-X6NX-BRCY-712R-FM"; 
+        var uuid = CrockfordUuid.Parse(expected); 
+        string actual = uuid.ToString(); 
+
+        Console.WriteLine(actual);
+        Console.WriteLine(uuid.ToString('-', 6)); 
+        Assert.Equal(expected, actual); 
     }
 }
